28ad82527191ccc73851a499ed4b5aaea81cad7b
AlessioMaria5 <alessiomaria5@gmail.com>
2022-09-28 16:51:44 +0200

    creazione card

---
+++ laravel-model-controller/resources/views/partials/moviecard.blade.php
@@ -0,0 +1,15 @@
+<section class="card">
+    <div class="img-wrap">
+        <img src="" alt="">
+    </div>
+
+    <hgroup>
+        <h2>Title</h2>
+        <h3>Original Title</h3>
+        <h3>Description</h3>
+        <h4>Nationality</h4>
+        <h5>Date</h5>
+    </hgroup>
+
+    <div class="movieVote">Vote</div>
+</section>
